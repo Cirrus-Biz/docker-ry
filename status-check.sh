dbdd069e2b4e5c40683d0cd34fcb4fa01b91ced3
noodleween <happyguy122x@gmail.com>
2022-08-11 13:09:41 -0600

    CB-179 refactor: status checking script

--- status-check.sh
+++ status-check.sh
@@ -1,20 +1,27 @@
-#!/bin/bash
+#!/bin/sh
 
 # script using aws cli for to check for stack status and wait until process ends to continue
+STACK="ac-ecs"
+aws cloudformation describe-stacks --stack-name=${STACK} > stack.txt 2>&1
 
-if aws cloudformation describe-stacks --stack-name="${STACK}"; then
-    # stack doesn't exist so line won't trigger
-    # if aws cloudformation describe-stacks --stack-name="${STACK}" | grep CREATE_IN_PROGRESS; then
-    #     aws cloudformation wait stack-create-complete --stack-name="${STACK}"
-    if aws cloudformation describe-stacks --stack-name="${STACK}" | grep UPDATE_IN_PROGRESS; then
-        aws cloudformation wait stack-update-complete --stack-name="${STACK}"
-    elif aws cloudformation describe-stacks --stack-name="${STACK}" | grep UPDATE_ROLLBACK_IN_PROGRESS; then
-        aws cloudformation wait stack-rollback-complete --stack-name="${STACK}"
-    elif aws cloudformation describe-stacks --stack-name="${STACK}" | grep DELETE_IN_PROGRESS; then
-        aws cloudformation wait stack-delete-complete --stack-name="${STACK}"
+if [ $? -ne 0 ]; then
+    echo "Stack ${STACK} does not yet exist"
+else
+    STATUS="`grep -m 1 "StackStatus" stack.txt`"
+    CREATE="CREATE_IN_PROGRESS"
+    UPDATE="UPDATE_IN_PROGRESS"
+    ROLLBACK="UPDATE_ROLLBACK_IN_PROGRESS"
+    DELETE="DELETE_IN_PROGRESS"
+
+    if echo ${STATUS} | grep ${CREATE};then
+        aws cloudformation wait stack-create-complete --stack-name=${STACK}
+    elif echo ${STATUS} | grep ${UPDATE};then
+        aws cloudformation wait stack-update-complete --stack-name=${STACK}
+    elif echo ${STATUS} | grep ${ROLLBACK};then
+        aws cloudformation wait stack-rollback-complete --stack-name=${STACK}
+    elif echo ${STATUS} | grep ${DELETE};then
+        aws cloudformation wait stack-delete-complete --stack-name=${STACK}
     else
         echo "No stack activity in progress!  You are clear to compose up"
     fi
-else
-    echo "Stack does not exist yet"
 fi
