c4628da7ecd7cfdb02d8a404bde345fca0cc986e
noodleween <happyguy122x@gmail.com>
2022-08-11 13:05:41 -0600

    CB-179 refactor: status checking script

--- status-check.sh
+++ status-check.sh
@@ -3,9 +3,10 @@
 # script using aws cli for to check for stack status and wait until process ends to continue
 
 if aws cloudformation describe-stacks --stack-name="${STACK}"; then
-    if aws cloudformation describe-stacks --stack-name="${STACK}" | grep CREATE_IN_PROGRESS; then
-        aws cloudformation wait stack-create-complete --stack-name="${STACK}"
-    elif aws cloudformation describe-stacks --stack-name="${STACK}" | grep UPDATE_IN_PROGRESS; then
+    # stack doesn't exist so line won't trigger
+    # if aws cloudformation describe-stacks --stack-name="${STACK}" | grep CREATE_IN_PROGRESS; then
+    #     aws cloudformation wait stack-create-complete --stack-name="${STACK}"
+    if aws cloudformation describe-stacks --stack-name="${STACK}" | grep UPDATE_IN_PROGRESS; then
         aws cloudformation wait stack-update-complete --stack-name="${STACK}"
     elif aws cloudformation describe-stacks --stack-name="${STACK}" | grep UPDATE_ROLLBACK_IN_PROGRESS; then
         aws cloudformation wait stack-rollback-complete --stack-name="${STACK}"
