068c316b6289332135db7fc949c557000da96180
noodleween <happyguy122x@gmail.com>
2022-08-11 12:54:03 -0600

    CB-179 refactor: status checking script

--- status-check.sh
+++ status-check.sh
@@ -1,25 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-aws cloudformation describe-stacks --stack-name=${STACK} > stack.txt 2>&1
+# script using aws cli for to check for stack status and wait until process ends to continue
 
-if [ $? -ne 0 ]; then
-    echo "Stack ${STACK} does not yet exist"
-else
-    NUM="`grep -m 1 "StackStatus" stack.txt`"
-    CREATE="CREATE_IN_PROGRESS"
-    UPDATE="UPDATE_IN_PROGRESS"
-    ROLLBACK="UPDATE_ROLLBACK_IN_PROGRESS"
-    DELETE="DELETE_IN_PROGRESS"
-
-    if echo ${NUM} | grep ${CREATE};then
-        aws cloudformation wait stack-create-complete --stack-name=${STACK}
-    elif echo ${NUM} | grep ${UPDATE};then
-        aws cloudformation wait stack-update-complete --stack-name=${STACK}
-    elif echo ${NUM} | grep ${ROLLBACK};then
-        aws cloudformation wait stack-rollback-complete --stack-name=${STACK}
-    elif echo ${NUM} | grep ${DELETE};then
-        aws cloudformation wait stack-delete-complete --stack-name=${STACK}
+if aws cloudformation describe-stacks --stack-name="ac-ecs"; then
+    if aws cloudformation describe-stacks --stack-name="ac-ecs" | grep CREATE_IN_PROGRESS; then
+        aws cloudformation wait stack-create-complete --stack-name="ac-ecs"
+    elif aws cloudformation describe-stacks --stack-name="ac-ecs" | grep UPDATE_IN_PROGRESS; then
+        aws cloudformation wait stack-update-complete --stack-name="ac-ecs"
+    elif aws cloudformation describe-stacks --stack-name="ac-ecs" | grep UPDATE_ROLLBACK_IN_PROGRESS; then
+        aws cloudformation wait stack-rollback-complete --stack-name="ac-ecs"
+    elif aws cloudformation describe-stacks --stack-name="ac-ecs" | grep DELETE_IN_PROGRESS; then
+        aws cloudformation wait stack-delete-complete --stack-name="ac-ecs"
     else
         echo "No stack activity in progress!  You are clear to compose up"
     fi
+else
+    echo "Stack does not exist yet"
 fi
