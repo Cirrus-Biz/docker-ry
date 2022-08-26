50431ccc5c3b3616296a418f7f065793efd124ea
noodleween <happyguy122x@gmail.com>
2022-08-26 16:36:49 -0600

    CB-182 Refactor: shell script

--- status-check.sh
+++ status-check.sh
@@ -3,25 +3,24 @@
 # script using aws cli for to check for stack status and wait until process ends to continue
 aws cloudformation describe-stacks --stack-name=${STACK} > stack.txt 2>&1
 
-if
-    # Status check vars
-    STATUS="`grep -m 1 "StackStatus" stack.txt`"
-    CREATE="CREATE_IN_PROGRESS"
-    UPDATE="UPDATE_IN_PROGRESS"
-    ROLLBACK="UPDATE_ROLLBACK_IN_PROGRESS"
-    DELETE="DELETE_IN_PROGRESS"
+# Status check vars
+STATUS="`grep -m 1 "StackStatus" stack.txt`"
+CREATE="CREATE_IN_PROGRESS"
+UPDATE="UPDATE_IN_PROGRESS"
+ROLLBACK="UPDATE_ROLLBACK_IN_PROGRESS"
+DELETE="DELETE_IN_PROGRESS"
 
-    if echo ${STATUS} | grep ${CREATE};then
-        aws cloudformation wait stack-create-complete --stack-name=${STACK}
-    elif echo ${STATUS} | grep ${UPDATE};then
-        aws cloudformation wait stack-update-complete --stack-name=${STACK}
-    elif echo ${STATUS} | grep ${ROLLBACK};then
-        aws cloudformation wait stack-rollback-complete --stack-name=${STACK}
-    elif echo ${STATUS} | grep ${DELETE};then
-        aws cloudformation wait stack-delete-complete --stack-name=${STACK}
-    else
-        echo "No stack activity in progress!  You are clear to compose up"
-    fi
+if echo ${STATUS} | grep ${CREATE};then
+    aws cloudformation wait stack-create-complete --stack-name=${STACK}
+elif echo ${STATUS} | grep ${UPDATE};then
+    aws cloudformation wait stack-update-complete --stack-name=${STACK}
+elif echo ${STATUS} | grep ${ROLLBACK};then
+    aws cloudformation wait stack-rollback-complete --stack-name=${STACK}
+elif echo ${STATUS} | grep ${DELETE};then
+    aws cloudformation wait stack-delete-complete --stack-name=${STACK}
+else
+    echo "No stack activity in progress!  You are clear to compose up"
 fi
+
 
 # debug comment
